66ffe02bc00bcb806512fbfe7b99b82febfb06a6
bashendriks <bashendriks14@msn.com>
2015-06-09 17:26:58 +0200

    info cards and pages

---
+++ app/js/directives/infoCard.js
@@ -0,0 +1,13 @@
+app.directive("infoCard", function($log) {
+	return{
+		restrict: 'A',
+		scope: {
+			title: "=",
+			content: "="
+		},
+		templateUrl: "templates/directive-templates/info-card.html",
+		controller: function($scope) {
+
+		}
+	}
+});
