5360f261729aa49d22db7f1a6165db7fc584e3b3
Rick Beumers <rick_beumers1994@hotmail.com>
2015-06-10 23:55:20 +0200

    Small fixes

--- app/js/controllers/ArtistCtrl.js
+++ app/js/controllers/ArtistCtrl.js
@@ -1,10 +1,8 @@
 app.controller('ArtistCtrl', function($scope, $stateParams, $log,$cordovaFile, $cordovaFileTransfer, $cordovaMedia, $ionicLoading, $sce, ArtistService, SpotifyService) {
 
-	var media;
-	$scope.playing = false;
-	$scope.loading = false;
 	$scope.spotify = false;
 
+	// Configure Spotify url
 	this.config = {
 		sources: [
       		{src: $sce.trustAsResourceUrl($scope.url), type: "audio/mpeg"}
@@ -15,45 +13,11 @@ app.controller('ArtistCtrl', function($scope, $stateParams, $log,$cordovaFile, $
 	ArtistService.getArtist($stateParams.id).then(function(data) {
 		$scope.artist = data;
 
+		// Get Spotify track
 		SpotifyService.get(data.name.__cdata).then(function(data) {
 			$scope.spotify = true;
-			$log.log(data);
 			$scope.url = data.preview_url;
 			$scope.track = data.name;
-						
-			// mediaPrepare(url);
 		});
 	});
-
-	// Prepare Media object, if available;
-	// function mediaPrepare(src) {
-	// 	media = new Media(src, function() {/* Success callback */}, null, function(status) {
-	// 		if(status == 1) {
-	// 			$scope.loading = true;
-	// 		} else {
-	// 			$scope.loading = false;
-	// 		}
-	// 	});
-	// }
-
-	// $scope.play = function() {
-	// 	$log.debug($scope.playing);
-
-	// 	if(!$scope.playing) {
-	// 		$scope.playing = true;
-	// 		media.play();
-	// 	} else {
-	// 		$scope.playing = false;
-	// 		media.pause();
-	// 	}
-	// }
-	
-	// $scope.$on('$ionicView.beforeLeave', function() {
-	// 	if($scope.playing) {
-	// 		media.pause();
-	// 	}
-	// });
-
-	
-
 });
