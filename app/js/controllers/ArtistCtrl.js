73c4b78b3a7fdc12364c5ced9f074daf48432d4f
rickbeumers <rick_beumers1994@hotmail.com>
2015-06-16 10:09:20 +0200

    hide buttons if no link available

--- app/js/controllers/ArtistCtrl.js
+++ app/js/controllers/ArtistCtrl.js
@@ -3,21 +3,32 @@ app.controller('ArtistCtrl', function($scope, $rootScope, $stateParams, $log, $s
 
 	// Configure Spotify url
 	this.config = {
-		sources: [
-      		{src: $sce.trustAsResourceUrl($scope.url), type: "audio/mpeg"}
-      	]
+		sources: [{src: $sce.trustAsResourceUrl($scope.url), type: "audio/mpeg"}]
 	};
 
 	angular.forEach($rootScope.artists.artist, function(data) {	
 		if($stateParams.id == data._id) {
 			$scope.artist = data;
 
-			// Get Spotify track
+			// Get Spotify information
 			SpotifyService.get(data.name.__cdata).then(function(res) {
 				$scope.spotify = true;
 				$scope.url = res.preview_url;
 				$scope.track = res.name;
+				$scope.artistUrl = res.artists[0].external_urls.spotify;
 			});
 		}
 	});
+
+	// Makes it possible to load links in browser outside of the app
+	$scope.InAppBrowser = function(e) {
+		e = e ||  window.event;
+        var element = e.target || e.srcElement;
+
+        if (element.tagName == 'A') {
+            window.open(element.href, "_system", "location=yes");
+            return false; // prevent default action and stop event propagation
+        }
+    };
+
 });
