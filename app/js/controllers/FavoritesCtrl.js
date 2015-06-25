dda6728d967b1027980a0f2eddecbde096149bd7
rickbeumers <rick_beumers1994@hotmail.com>
2015-06-25 12:34:59 +0200

    no favourites yet updated so it shows only when you have no favourites & animation

--- app/js/controllers/FavoritesCtrl.js
+++ app/js/controllers/FavoritesCtrl.js
@@ -1,6 +1,29 @@
-app.controller('FavoritesCtrl', function($scope, $rootScope, $log, SpotifyService) {
-
+app.controller('FavoritesCtrl', function($scope, $rootScope, $log, SpotifyService, ArtistService) {
+	// Create playlist from favourites
 	SpotifyService.createPlaylist().then(function(data) {
 		$scope.playlist = data;
 	});
+
+	ArtistService.getFavourites().then(function(data) {
+		$log.log(data.artists)
+		if(data.artists.length > 0) {
+
+			$scope.favourited = true;
+		} else {
+			$scope.favourited = false;
+		}
+	});
+
+	$rootScope.$on('favourited', function() {
+		ArtistService.getFavourites().then(function(data) {
+			$log.log(data.artists)
+			if(data.artists.length > 0) {
+
+				$scope.favourited = true;
+			} else {
+				$scope.favourited = false;
+			}
+		});
+	})
+
 });
