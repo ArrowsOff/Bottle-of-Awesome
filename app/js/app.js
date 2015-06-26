ab26ae19c68cb7732ddd195b06e7e28857bf11b1
bashendriks <bashendriks14@msn.com>
2015-06-26 16:15:14 +0200

    remove mc from timetable

--- app/js/app.js
+++ app/js/app.js
@@ -76,7 +76,7 @@ app.run(function($ionicPlatform, $rootScope, $log, $q, ImgCache, ArtistService, 
             // remove invalide date and vj
             angular.forEach(data.artist, function(artist, key) {
                 var name = artist.name.__cdata;
-                if (name == "Roog" || name == "Michel De Hey" || name == "STV-Visuals") {
+                if (name == "Roog" || name == "Michel De Hey" || name == "STV-Visuals" || name == "Mc Robbie Rise") {
                     data.artist.splice(key);
                 }
             });
