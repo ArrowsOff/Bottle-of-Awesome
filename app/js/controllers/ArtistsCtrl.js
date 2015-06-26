8ceb4a9151da3f244258135a27b168c089c08f8f
rickbeumers <rick_beumers1994@hotmail.com>
2015-06-26 16:36:54 +0200

    height aanpassing

--- app/js/controllers/ArtistsCtrl.js
+++ app/js/controllers/ArtistsCtrl.js
@@ -10,6 +10,6 @@ app.controller('ArtistsCtrl', function($scope, $rootScope, $log, ArtistService, 
 	AreaService.getAreas().then(function(data) {
         $scope.stages = data.stage;
     }).catch(function(err) {
-        $log.error("Error getting Areas:", err);
+        $log.error("Error getting areas:", err);
     });
 });
