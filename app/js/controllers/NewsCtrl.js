70e4923324ba7d0605748ba4bbdfac240ac7ba35
bashendriks <bashendriks14@msn.com>
2015-06-26 13:46:52 +0200

    timer check

--- app/js/controllers/NewsCtrl.js
+++ app/js/controllers/NewsCtrl.js
@@ -4,8 +4,8 @@ app.controller('NewsCtrl', function ($scope, $log, $http) {
 
 	// Function for Countdown
 	function countdown() {
-		$scope.counter = moment().countdown("2015/7/11");
-		if($scope.counter.days < 10){
+		$scope.counter = moment().countdown("2015/7/11 11:00:00");
+        if($scope.counter.days < 10){
         	$scope.counter.days= "0" + $scope.counter.days;
         }
         if($scope.counter.hours < 10){
@@ -17,7 +17,17 @@ app.controller('NewsCtrl', function ($scope, $log, $http) {
         if($scope.counter.seconds < 10){
         	$scope.counter.seconds= "0" + $scope.counter.seconds;
         }
+
+        $scope.festival = moment().countdown("2015/7/11 23:15:00");
 	}
+
+    $scope.check = function() {
+        if ($scope.counter.value < 0 && $scope.festival.value >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 	// Update countdown every second.
 	setInterval(function(){
