0b0e6f4b9b25a23d6b099d0b16a0dd083bd76cf9
rickbeumers <rick_beumers1994@hotmail.com>
2015-06-26 09:52:26 +0200

    version 0.0.3

--- app/js/controllers/ScheduleCtrl.js
+++ app/js/controllers/ScheduleCtrl.js
@@ -1,46 +1,16 @@
 app.controller('ScheduleCtrl', function($scope, $log, $ionicScrollDelegate) {
-
-	// AreaService.getAreas().then(function(data){
- //        $log.debug(data.area);
-
- //        $scope.areas = data.area;
-
- //    }).catch(function(err){
- //        $log.error("Error getting Areas:", err);
- //    });
-
-    // $scope.toArea = function(id) {
-    // 	angular.forEach($scope.areas, function(area){
-    // 		if(area._id === id) {
-    // 			$log.log(area.title.toString());
-    // 			return area.title.toString();
-    // 		}
-    // 	})
-    // }
-    // $log.log($rootScope.artists)
-    // angular.forEach($rootScope.artists.artist, function(artist) {
-    // 	// $log.log(artist.stage_name)
-
-    // 	if(artist.stage_name === undefined) {
-    // 		$log.error(artist.stage_id)
-    // 	}
-    // })
     $scope.calcWidthSC = screen.width;
     $log.log($scope.calcWidthSC);
     $scope.calcHeightSC = screen.height;
     $log.log($scope.calcHeightSC);
 
     $scope.calculateHeight = function(artist) {
-        // $log.log($scope.toTime(artist.end_time), $scope.toTime(artist.start_time))
-        // $log.log(parseInt($scope.toTime(artist.end_time), 10 ));
-
         var date1 = new Date(artist.start_time);
         var date2 = new Date(artist.end_time);
         var diffMs = (date2 - date1); // milliseconds between now & Christmas
         var diffHrs = Math.round((diffMs % 86400000) / 3600000); // hours
         var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
         var result;
-
 
         if(diffHrs == 2) {
             result = diffHrs*60 - diffMins;
@@ -53,18 +23,6 @@ app.controller('ScheduleCtrl', function($scope, $log, $ionicScrollDelegate) {
             result = diffMins;
         }
 
-        $log.log(artist.name.__cdata, result)
-
         return result*1.5;
     }
-
-    var delegate;
-    var secondDelegate;
-
-    $scope.scrolling = function() {
-      delegate = $ionicScrollDelegate.$getByHandle('bottom');
-      secondDelegate = $ionicScrollDelegate.$getByHandle('top');
-      secondDelegate.scrollTo(delegate.getScrollPosition().left);
-    }
-
 });
