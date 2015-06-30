d283aa0cf2642ac5727a95439267fa9fd09a29d9
josvdmanakker <j.vandemanakker@student.fontys.nl>
2015-06-30 14:14:15 +0200

    map img center

--- app/js/controllers/MapCtrl.js
+++ app/js/controllers/MapCtrl.js
@@ -1,3 +1,10 @@
-app.controller('MapCtrl', function ($scope) {
+app.controller('MapCtrl', function ($scope, $ionicScrollDelegate, $log) {
+    var delegate;
 
+    $scope.mapscroll = function() {
+        delegate = $ionicScrollDelegate.$getByHandle('mapImage');
+        //$log.log(delegate);
+        $log.log(delegate.getScrollPosition());
+        delegate.scrollTo(350,350);
+    }
 });
