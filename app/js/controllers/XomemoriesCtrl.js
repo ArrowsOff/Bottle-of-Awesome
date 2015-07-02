c3736fca3ab04b1dd0e8cc7afbcdb00ae53fa436
Rick Beumers <rick_beumers1994@hotmail.com>
2015-07-03 01:53:54 +0200

    fixed all database stuff. rest is on server side

--- app/js/controllers/XomemoriesCtrl.js
+++ app/js/controllers/XomemoriesCtrl.js
@@ -1,4 +1,4 @@
-app.controller('XOmemoriesCtrl', function($scope, $log){
+app.controller('XOmemoriesCtrl', function($scope, $rootScope, $log, $cordovaImagePicker){
     var calcHeight = (document.getElementById('memories').offsetHeight) -100;
     $scope.memoryHeight = calcHeight;
     $scope.uploadCheck = true;
@@ -8,5 +8,23 @@ app.controller('XOmemoriesCtrl', function($scope, $log){
     } else {
         $scope.itemHeight = ((calcHeight / 2) -20);
     }
+
+    $scope.openImagePicker = function() {
+        var options = {
+            maximumImagesCount: 10,
+            width: 800,
+            height: 800,
+            quality: 80
+        };
+
+        $cordovaImagePicker.getPictures(options).then(function (results) {
+            $rootScope.memories = {
+                pictures: results
+            };
+
+        }, function(error) {
+            // error getting photos
+        });
+    };
 
 });
--- app/js/controllers/XomemoriesNextCtrl.js
+++ app/js/controllers/XomemoriesNextCtrl.js
@@ -1,7 +1,10 @@
-app.controller('xomemoriesNextCtrl', function($scope){
+app.controller('xomemoriesNextCtrl', function($scope, $rootScope, $log, $http){
     var calcHeight = (document.getElementById('memoriesNext').offsetHeight) -100;
     $scope.memoryHeight = calcHeight;
     $scope.uploadCheck = true;
+
+    $rootScope.memories.description = [];
+    $rootScope.memories.quotes = [];
 
     if ($scope.uploadCheck === true) {
         $scope.itemHeight = ((calcHeight / 2) -50);
@@ -11,10 +14,55 @@ app.controller('xomemoriesNextCtrl', function($scope){
 
     $scope.clearQuote = function(num) {
         document.getElementById('quote'+num).value = '';
-    }
+    };
 
     $scope.clearPhotoText = function(num) {
         document.getElementById('phototext'+num).value = '';
-    }
+    };
+
+    $scope.upload = function() {
+        $log.log("Starting to upload");
+        $log.log($rootScope.memories);
+
+        var options;
+        var description;
+        angular.forEach($rootScope.memories.quotes, function(qu){
+            $log.log(qu);
+            $http.post('http://app.xofestival.nl/api/quotes.php', {quote: qu}).success(function(data){
+                $log.log("quote:", data);
+            });
+        });
+
+        angular.forEach($rootScope.memories.pictures, function(image, key) {
+            $log.log(key, image);
+            $log.log($rootScope.memories.description[key]);
+            options = {
+                fileKey: "file",
+                fileName: "image"+key+".jpg",
+                mimeType: "image/jpeg"
+            };
+            if($rootScope.memories.description[key] !== '') {
+                description = {
+                    desc: $rootScope.memories.description[key],
+                    imageId: key
+                };
+
+                $http.post('http://app.xofestival.nl/api/description.php', description).success(function(data){
+                    $log.log("description:", data);
+                });
+            }
+
+
+            $cordovaFileTransfer.upload("http://app.xofestival.nl/api/upload.php", image, options).then(function(result) {
+                $log.log("SUCCESS: " + JSON.stringify(result.response));
+            }, function(err) {
+                $log.error("ERROR: " + JSON.stringify(err));
+            }, function (progress) {
+                // constant progress updates
+                $log.log("PROGESS:", progress);
+            });
+        });
+
+    };
 
 });
