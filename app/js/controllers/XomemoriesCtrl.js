e4325db6a8524c8246c531f0583abb454ba6247b
bashendriks <bashendriks14@msn.com>
2015-07-02 09:36:39 +0200

    xo memories styling

--- app/js/controllers/XomemoriesCtrl.js
+++ app/js/controllers/XomemoriesCtrl.js
@@ -1,1 +1,12 @@
-app.controller('XOmemoriesCtrl', function($scope){});
+app.controller('XOmemoriesCtrl', function($scope, $log){
+    var calcHeight = (document.getElementById('memories').offsetHeight) -100;
+    $scope.memoryHeight = calcHeight;
+    $scope.uploadCheck = true;
+
+    if ($scope.uploadCheck === true) {
+        $scope.itemHeight = ((calcHeight / 2) -50);
+    } else {
+        $scope.itemHeight = ((calcHeight / 2) -20);
+    }
+
+});
--- app/js/controllers/XomemoriesNextCtrl.js
+++ app/js/controllers/XomemoriesNextCtrl.js
@@ -1,1 +1,20 @@
-app.controller('xomemoriesNextCtrl', function($scope){});
+app.controller('xomemoriesNextCtrl', function($scope){
+    var calcHeight = (document.getElementById('memoriesNext').offsetHeight) -100;
+    $scope.memoryHeight = calcHeight;
+    $scope.uploadCheck = true;
+
+    if ($scope.uploadCheck === true) {
+        $scope.itemHeight = ((calcHeight / 2) -50);
+    } else {
+        $scope.itemHeight = ((calcHeight / 2) -20);
+    }
+
+    $scope.clearQuote = function(num) {
+        document.getElementById('quote'+num).value = '';
+    }
+
+    $scope.clearPhotoText = function(num) {
+        document.getElementById('phototext'+num).value = '';
+    }
+
+});
